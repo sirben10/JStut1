c3a507538263635af09f23ee4dc21d1409c353c1
SirBen <akawuben@gmail.com>
2026-03-13 09:33:49 +0100

    Random Number Generator for unique ID Number

--- RandNumGenerator/index.js
+++ RandNumGenerator/index.js
@@ -1,10 +1,12 @@
 const myButton = document.getElementById("myButton");
 const myLabel = document.getElementById("myLabel");
-const min = 1;
-const max = 6;
+const min = 10000;
+const max = 60000;
 let randomNum;
+let textBefore = 'AOE';
+let year = new Date().getFullYear();
 
 myButton.addEventListener("click", () => {
     randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    myLabel.textContent = randomNum;
+    myLabel.textContent = textBefore + randomNum + year;
 });
